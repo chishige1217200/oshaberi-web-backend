accf34617074dfc50920dafa1532348b7a97fc23
chishige1217200 <daichicchi01@gmail.com>
2025-09-30 22:48:22 +0900

    Add: 基本的なAPI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+import db
 
 app = FastAPI()
 
@@ -7,6 +8,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/chats/")
+async def get_chats():
+    return db.get_chats()
+
+@app.get("/all-messages/")
+async def get_all_messages():
+    return db.get_all_messages()
+
+@app.get("/messages/{chat_id}")
+async def get_messages(chat_id: int):
+    return db.get_messages(chat_id)
+
+@app.post("/chat/")
+async def post_chat(data: dict):
+
+    return {"received": data}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
