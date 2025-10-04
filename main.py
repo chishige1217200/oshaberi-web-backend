17fbd604ce11a49b7716f648a5ebcd35c1e9ec9e
chishige1217200 <daichicchi01@gmail.com>
2025-10-04 18:52:35 +0900

    Add: CORS制限解除とstaticルートの追加

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
 import uvicorn
 import db
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
